717e194f4938b3e73a0d8e3e74b88730d6b023a9
sajjaddg <sajjaddg@gmail.com>
2021-11-16 17:38:46 +0330

    add gRPC project for discount service

--- src/Services/Discount/Discount.gRPC/Startup.cs
+++ src/Services/Discount/Discount.gRPC/Startup.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.gRPC.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Discount.gRPC
 {
@@ -16,6 +14,7 @@ namespace Discount.gRPC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddGrpc();
         }
 
